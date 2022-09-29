ad3a77e56feb3df5288f7a5f636c503263777079
baland_h <baland_h@etna-alternance.net>
2022-09-29 17:50:05 -0400

    modif

--- lib.ts
+++ lib.ts
@@ -1,4 +1,5 @@
-
+import Gamer from './gamer';
+import Monster from './monster';
 
 function displaymonster(monster: Monster) {
   console.log(monster);
@@ -6,6 +7,13 @@ function displaymonster(monster: Monster) {
 
 function displaygamers(gamer: Gamer) {
   console.log(gamer);
+}
+
+function damagemonster(monster: Monster) {
+  monster.HP -= damage;
+}
+function damagegamer(gamer: Gamer) {
+  gamer.HP -= damage;
 }
 
 export default ('displaymonster displaygamers');
