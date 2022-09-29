b941089140449d5354a10fc61c340baa2d64c14a
baland_h <baland_h@etna-alternance.net>
2022-09-29 12:59:04 -0400

    debut

--- README.md
+++
@@ -1,2 +0,0 @@
-# Groupe de baland_h 988348
-
---
+++ hyrule_castle.js
@@ -0,0 +1,6 @@
+var numberApples = 50;
+var remainApples = numberApples % 6;
+var givenApples = numberApples / 6;
+console.log(remainApples);
+console.log(givenApples);
+console.log("chaque person va recevoir ".concat(givenApples, " apples."));
