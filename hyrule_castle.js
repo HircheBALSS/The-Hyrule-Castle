d90d2eef8f657d7951f29d37e45c483f2a3994ab
baland_h <baland_h@etna-alternance.net>
2022-10-01 05:55:47 -0400

    remove2

--- hyrule_castle.js
+++ hyrule_castle.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var readline = require('readline-sync');
 var fs = require('fs');
-var clc = require("cli-color");
+var clc = require('cli-color');
 var main = fs.readFileSync('./players.json', 'utf-8');
 var contentmain = JSON.parse(main);
 var content = fs.readFileSync('./enemies.json', 'utf-8');
@@ -13,13 +13,38 @@ var devil = mainContent[11].name;
 var hero = contentmain[0].name;
 var Lifedevil = mainContent[11].hp;
 var Lifehero = contentmain[0].hp;
+var STRDevil = mainContent[11].str;
+var STRhero = contentmain[0].str;
 var i = 1;
 var person = 'FIGHT';
 while (i < 11) {
     console.log("======== ".concat(person, " ").concat(i, "  ========="));
-    console.log(clc.yellow(devil));
-    console.log("HP__________ ".concat(Lifedevil), '\n', '\n', '\n');
-    console.log(clc.red(hero), '\n');
-    console.log("HP_____________ ".concat(Lifehero), '\n');
+    while (Lifedevil > 0 || Lifehero > 0) {
+        console.log(clc.yellow(devil));
+        console.log("HP__________ ".concat(Lifedevil, "/").concat(mainContent[11].hp), '\n', '\n', '\n');
+        console.log(clc.red(hero), '\n');
+        console.log("HP_____________ ".concat(Lifehero, "/").concat(contentmain[0].hp), '\n');
+        var Attack = 1;
+        var Heal = 2;
+        console.log('------ Options -----------');
+        var answer = readline.question('1. Attack   2. Heal \n');
+        answer = parseInt(answer, 10);
+        console.log(answer);
+        if (answer === Attack) {
+            Lifedevil -= STRhero;
+            Lifehero -= STRDevil;
+            console.clear();
+            console.log(clc.green('you have chosen to Attack'));
+        }
+        else if (answer === Heal) {
+            console.clear();
+            console.log(clc.green('you have chosen to Heal'));
+        }
+        if (Lifedevil < 0) {
+            console.log('vous avez gagnée la partie');
+            Lifedevil = mainContent[11].hp;
+            break;
+        }
+    }
     i += 1;
 }
--- hyrule_castle.ts
+++ hyrule_castle.ts
@@ -1,10 +1,11 @@
 import Gamer from './gamer';
 import Monster from './monster';
 import lib from './lib';
+import { Console } from 'console';
 
 const readline = require('readline-sync');
 const fs = require('fs');
-const clc = require("cli-color");
+const clc = require('cli-color');
 
 const main = fs.readFileSync('./players.json', 'utf-8');
 const contentmain = JSON.parse(main);
@@ -13,22 +14,55 @@ const mainContent = JSON.parse(content);
 const objet = fs.readFileSync('./bosses.json', 'utf-8');
 const objetMain = JSON.parse(objet);
 
-let devil = mainContent[11].name;
-let hero = contentmain[0].name;
+const devil = mainContent[11].name;
+const hero = contentmain[0].name;
 let Lifedevil = mainContent[11].hp;
 let Lifehero = contentmain[0].hp;
-
+const STRDevil = mainContent[11].str;
+const STRhero = contentmain[0].str;
 
 let i = 1;
 
-let person: string = 'FIGHT';
-while(i < 11){
-    console.log(`======== ${person} ${i}  =========`);
-    
-console.log(clc.yellow(devil));
-console.log(`HP__________ ${Lifedevil}`, '\n', '\n', '\n');
-console.log(clc.red(hero), '\n');
-console.log(`HP_____________ ${Lifehero}`, '\n');
+const person: string = 'FIGHT';
+while (i < 11) {
+  console.log(`======== ${person} ${i}  =========`);
 
-    i+= 1
+  while (Lifedevil > 0 || Lifehero > 0) {
+    console.log(clc.yellow(devil));
+    console.log(`HP__________ ${Lifedevil}/${mainContent[11].hp}`, '\n', '\n', '\n');
+    console.log(clc.red(hero), '\n');
+    console.log(`HP_____________ ${Lifehero}/${contentmain[0].hp}`, '\n');
+
+    const Attack = 1;
+    const Heal = 2;
+
+    console.log('------ Options -----------');
+
+    let answer = readline.question('1. Attack   2. Heal \n');
+    answer = parseInt(answer, 10);
+    console.log(answer);
+
+    if (answer === Attack) {
+      Lifedevil -= STRhero;
+      Lifehero -= STRDevil;
+      console.clear();
+      console.log(clc.green('you have chosen to Attack'));
+    } else if (answer === Heal) {
+      Lifehero += ()
+      console.clear();
+      console.log(clc.green('you have chosen to Heal'));
+    }
+
+    if (Lifedevil < 0) {
+      console.log('vous avez gagnée la partie');
+      Lifedevil = mainContent[11].hp;
+      break;
+    } 
+    Console.clear();
+    if (Lifehero < 0) {console.log(clc.red('GAME OVER');
+    break;
+}
+  }
+
+  i += 1;
 }
